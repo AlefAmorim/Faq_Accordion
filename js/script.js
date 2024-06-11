adaa9abfe4a27275a3605c379698455785e06135
Alef A. Santos <alefsantos8730@gmail.com>
2024-06-11 14:06:59 -0300

    commit some changes in script,html,styles css and css medias queries

--- js/script.js
+++ js/script.js
@@ -1,43 +1,13 @@
-let button=document.querySelectorAll('button');
-let paragraph=document.querySelectorAll("p");
-
-button.forEach(function(buttons) {
-        buttons.addEventListener("click", (show)=>{});
-});
-
-button[0].onclick=(show)=>{
-        if(paragraph[0].hidden===true){
-            paragraph[0].hidden=false;
-            button[0].className="minus";
+let buttons=document.querySelectorAll("button").forEach((button)=>{
+    button.addEventListener("click",()=>{
+        if(!button.previousElementSibling.classList.contains('hiddenAnswer')){
+            button.previousElementSibling.classList.add('hiddenAnswer')
+            button.classList.remove('minus')
+            button.classList.add('plus')
         }else{
-            paragraph[0].hidden=true;
-            button[0].className="plus";
+                button.previousElementSibling.classList.remove('hiddenAnswer')
+                button.classList.remove('plus')
+                button.classList.add('minus')
         }
-}
-button[1].onclick=(show)=>{
-    if(paragraph[1].hidden===true){
-        paragraph[1].hidden=false;
-        button[1].className="minus";
-    }else{
-        paragraph[1].hidden=true;
-        button[1].className="plus";
-    }
-}
-button[2].onclick=(show)=>{
-    if(paragraph[2].hidden===true){
-        paragraph[2].hidden=false;
-        button[2].className="minus";
-    }else{
-        paragraph[2].hidden=true;
-        button[2].className="plus";
-    }
-}
-button[3].onclick=(show)=>{
-    if(paragraph[3].hidden===true){
-        paragraph[3].hidden=false;
-        button[3].className="minus";
-    }else{
-        paragraph[3].hidden=true;
-        button[3].className="plus";
-    }
-}
+    })
+})
